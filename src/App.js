2e2ef08a38bc28cc551a4b9fee509a0c3f11f84c
covenantebube <covenantebube1@gmail.com>
2023-04-07 09:52:35 -0700

    mapping array to object init

--- src/App.js
+++ src/App.js
@@ -4,48 +4,29 @@ import './App.css';
 
 class App extends Component {
   constructor(){
-    super();
+  super();
     
   this.state = {
-    name: {firstName:'Covenant', lastName: 'Ebube'},
-    company: 'Glodev' ,
-  };
+    
+      monster1: {name:'Covenant',
+      },
+      
+      monster2: {name:'Blessed',
+      },
+      
+      monster3: {name:'praise',
+      },
+      }; 
 
-  }  
-
+    }
 
   render (){ 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
-          <button onClick={()=>{
-            this.setState(
-              ()=> {
-              return {
-                name : {firstName:'Innocent', lastName: 'Covenant'}
-              }
-            }, ()=>{
-
-            console.log(this.state)
-
-            })
-
-              }   
-          }
-           >change Name</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className="App"> 
+      <h1>{this.state.monster1.name}</h1>
+      <h1>{this.state.monster2.name}</h1>
+      <h1>{this.state.monster3.name}</h1>
+      
+  </div>
   }
 }
 
