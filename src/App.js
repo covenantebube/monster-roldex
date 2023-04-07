19170542d70f0980cb24d65d3002f83133406db0
covenantebube <covenantebube1@gmail.com>
2023-04-07 09:36:59 -0700

    setState and secondary callback

--- src/App.js
+++ src/App.js
@@ -21,9 +21,20 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
           <button onClick={()=>{
-            this.setState({name : {firstName:'Innocent', lastName: 'Covenant'}});
+            this.setState(
+              ()=> {
+              return {
+                name : {firstName:'Innocent', lastName: 'Covenant'}
+              }
+            }, ()=>{
+
             console.log(this.state)
-          }}>change Name</button>
+
+            })
+
+              }   
+          }
+           >change Name</button>
           <a
             className="App-link"
             href="https://reactjs.org"
