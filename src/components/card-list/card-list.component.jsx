0bab334be7a1a9a7e4f9cfef7835a0af6841dbf5
covenantebube <covenantebube1@gmail.com>
2023-04-09 08:47:25 -0700

    cardlist style 3

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,8 @@
 import { render } from "@testing-library/react";
 import { Component } from "react";
 
+import './card-list.styles.css'
+
 class CardList extends Component {
         render(){
           const {monsters} = this.props;
