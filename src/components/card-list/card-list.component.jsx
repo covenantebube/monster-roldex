62325f834a242428df389dfc4874fd95b05c6e28
covenantebube <covenantebube1@gmail.com>
2023-04-11 04:05:07 -0700

    functional component 3

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,26 +1,16 @@
 import { render } from "@testing-library/react";
-import { Component } from "react";
 
 import Card from "../card/card.component";
 import './card-list.styles.css'
 
-class CardList extends Component {
-        render(){
-          const {monsters} = this.props;
-               
-          return (
-            <div className="card-list">
+const CardList = ( {monsters})=> (
+       <div className="card-list">
                 {monsters.map((monster) => {
-
-                 return  (
+         return  (
                     <Card  monster ={monster}/>
                  ); 
                  })}
               </div>
-              )
-                
-                
-        }
+              );
 
-}
 export default CardList;
